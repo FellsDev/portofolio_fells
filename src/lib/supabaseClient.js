207819f7310fa5dc5f67520e0ac7eecc95cd3569
laFells <hanif1727disini@gmail.com>
2025-09-30 07:44:01 +0700

    refactor:vercel

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,11 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseURL || !supabaseAnonKey) {
-  throw new Error("Missing supabase environment variable. please cheak your");
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
 }
 
-export const supabase = createClient(supabaseURL, supabaseAnonKey);
-
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
